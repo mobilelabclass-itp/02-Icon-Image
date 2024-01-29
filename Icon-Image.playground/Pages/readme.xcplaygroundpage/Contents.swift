d05eb6b07f3f76d05583228c4c3e1060c70b6262
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2024-01-28 20:47:21 -0500

    readme

---
+++ Icon-Image.playground/Pages/readme.xcplaygroundpage/Contents.swift
@@ -0,0 +1,4 @@
+//# [02-Icon-Image](https:github.com/molab-itp/02-Icon-Image)
+// - Using SwiftUI in a playground.
+// - Preparing 1024x1024 icon images
+                  
--- Icon-Image.playground/Resources/README.md
+++
@@ -1,5 +0,0 @@
-# [02-Icon-Image](https://github.com/mobilelabclass-itp/02-Icon-Image)
-
-- Using SwiftUI in a playground.
-
-- Preparing 1024x1024 icon images
